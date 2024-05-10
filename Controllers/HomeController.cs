b20c55bac4c1772e264ad6645cf890d463f0135b
Clement Omolayo <111860220+technoclem@users.noreply.github.com>
2024-05-10 07:29:09 +0100

    Update HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,12 +1,7 @@
-using FastFood.Dto;
 using FastFood.Models;
 using FastFood.Service.Interface;
-using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
-using System.Net.NetworkInformation;
-using System.Reflection;
-using System.Security.Claims;
 
 namespace FastFood.Controllers
 {
